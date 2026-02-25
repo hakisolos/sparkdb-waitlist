f3e391e2a4179dc3da7f031adeb8222691ebd6b9
hakisolos <maxwellexcel174@gmail.com>
2026-02-25 20:21:13 +0100

    uvicorn support fix

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-from fastapi import FastAPI
-from src.controller import Home,joinWaitlist,getWaiters
-from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.get("/")(Home)
-app.post("/waitlist/join")(joinWaitlist)
-app.get("/waiters")(getWaiters)
+from src.main import app
+import uvicorn
+if __name__ == "__main__":
+    uvicorn.run(
+        "src.main:app",
+        host="0.0.0.0",
+        port=8000,
+        workers=4
+    )
